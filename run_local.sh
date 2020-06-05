bc4028183a9f3b118815b5bfd89bec13e4fe1463
Priya Ranjan <pranjan77@gmail.com>
2020-06-05 10:03:45 -0400

    delete ununsed files

--- run_local.sh
+++
@@ -1,8 +0,0 @@
-docker container kill $(docker ps -q)
-docker-compose build 
-docker run -v /Users/priyaranjan/kbasecode/VariationFileServ/test_local/workdir:/kb/module/work   -d -i -t -p 5000:5000   variationfileserv_web:latest 
-#rm -rf test_local/workdir/*
-#python test.py
-#perl t.pl
-#du -a test_local/workdir/
-
--- t.pl
+++
@@ -1,7 +0,0 @@
-
-use strict;
-my $val = `(docker ps -a -q)`;
-my @v = split ("\n", $val);
-print $v[0];
-my $ix = `(docker logs $v[0])`;
-print $ix;
